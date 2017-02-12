9fd5983acb00be01a870b51260ad1c00ffdd00df
Cherwin Nooitmeer <cherwin@gmail.com>
2017-02-12 17:53:23 +0000

    Fix mixed indentation

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 # Module variables
 
 variable "hostname" {
-	description = "Label for droplet within terraform and the droplet's hostname"
+    description = "Label for droplet within terraform and the droplet's hostname"
 }
 variable "image" {
-	default = "ubuntu-14-04-x64"
-	description = "See https://developers.digitalocean.com/#list-all-images for a list of possible values"
+    default = "ubuntu-14-04-x64"
+    description = "See https://developers.digitalocean.com/#list-all-images for a list of possible values"
 }
 variable "region" {
-	default = "lon1"
-	description = "See https://developers.digitalocean.com/#regions for a list of possible values"
+    default = "lon1"
+    description = "See https://developers.digitalocean.com/#regions for a list of possible values"
 }
 variable "size" {
-	default = "512mb"
-	description = "See https://developers.digitalocean.com/#sizes for a list of possible values"
+    default = "512mb"
+    description = "See https://developers.digitalocean.com/#sizes for a list of possible values"
 }
 variable "private_networking" {
-	default = "true"
-	description = "See https://www.digitalocean.com/company/blog/introducing-private-networking/"
+    default = "true"
+    description = "See https://www.digitalocean.com/company/blog/introducing-private-networking/"
 }
 variable "ssh_fingerprint" {
-	description = "See module read-me for details"
+    description = "See module read-me for details"
 }
 
 
@@ -33,7 +33,7 @@ resource "digitalocean_droplet" "droplet" {
     size = "${var.size}"
     private_networking = "${var.private_networking}"
     ssh_keys = [
-    	"${var.ssh_fingerprint}"
+        "${var.ssh_fingerprint}"
     ]
 }
 
